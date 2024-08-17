07a15df7c58ac36fb5f766b6fda1500d422cc669
Aryabhatta Dey <aryabhattadey35@gmail.com>
2024-08-17 13:32:24 +0530

    Adds support for config files. (#3)
    Uses config parser to read the config file and in case it is absent then
    instantiates with default values.

--- ganga_ai/llm.py
+++ ganga_ai/llm.py
@@ -2,7 +2,7 @@ from llama_index.llms.ollama import Ollama
 from llama_index.core.base.llms.types import ChatMessage, ChatResponse
 from typing import List
 
-def generate_response(context: List[ChatMessage]) -> ChatResponse:
-    llm = Ollama (model="tinyllama", request_timeout=300)
+def generate_response(context: List[ChatMessage], model_name: str) -> ChatResponse:
+    llm = Ollama(model=model_name, request_timeout=300)
     result = llm.chat(context)
     return result
