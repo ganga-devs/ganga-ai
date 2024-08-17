1e5d74075893b040759822c6a05b1472160e70cd
Aryabhatta Dey <aryabhattadey35@gmail.com>
2024-08-17 17:33:33 +0530

    Formatted the files with black and added it as a dev dependency.

--- ganga_ai/llm.py
+++ ganga_ai/llm.py
@@ -2,7 +2,8 @@ from llama_index.llms.ollama import Ollama
 from llama_index.core.base.llms.types import ChatMessage, ChatResponse
 from typing import List
 
+
 def generate_response(context: List[ChatMessage]) -> ChatResponse:
-    llm = Ollama (model="tinyllama", request_timeout=300)
+    llm = Ollama(model="tinyllama", request_timeout=300)
     result = llm.chat(context)
     return result
